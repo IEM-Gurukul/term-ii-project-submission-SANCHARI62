e161197d32dc74c871a1f516cd4fdcfcfe75e1ab
SANCHARI62 <singharoyksanchari@gmail.com>
2026-03-29 20:04:17 +0530

    Add files via upload
    adding room

---
+++ src/Room.java
@@ -0,0 +1,31 @@
+public abstract class Room {
+    private int roomNumber;
+    private double price;
+    private boolean isBooked;
+
+    public Room(int roomNumber, double price) {
+        this.roomNumber = roomNumber;
+        this.price = price;
+        this.isBooked = false;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public boolean isBooked() {
+        return isBooked;
+    }
+
+    public void setBooked(boolean booked) {
+        isBooked = booked;
+    }
+
+    public abstract String getRoomType();
+    
+    public abstract void displayDetails();
+}
